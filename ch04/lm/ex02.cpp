c3f2a08c4245d72acffa56d378bd5204df887acd
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-06-22 18:58:43 +0200

    ???

--- ch04/lm/ex02.cpp
+++ ch04/lm/ex02.cpp
@@ -4,6 +4,6 @@
 
 TEST(LMCh04Ex02, willHandleSmallInput)
 {
-    auto median = Median<double>({1.0, 3.0, 5.0, 7.1, 9.2}); 
+    auto median = Median<double>({1.0, 3.0, 5.0, 7.1, 9.2});
     ASSERT_EQ(5.0, median.get());
 }
