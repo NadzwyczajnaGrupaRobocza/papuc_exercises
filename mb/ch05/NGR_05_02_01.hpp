6f91c9851ccfcbacad5723f7171d0ed6758cef86
Feldmarszalekduda <Feldmarszalekduda@gmail.com>
2016-07-09 12:47:29 +0200

    Removing comment

--- mb/ch05/NGR_05_02_01.hpp
+++ mb/ch05/NGR_05_02_01.hpp
@@ -7,8 +7,6 @@ constexpr double FAHRENHEIT_MIN = -459.67;
 class TemperatureConverter
 {
 public:
-//	TemperatureConverter();
-
 	double celsiusToKelvin(const double& celsius);
 	double kelvinToCelsius(const double& celsius);
 	double celsiusToFahrenheit(const double& celsius);
