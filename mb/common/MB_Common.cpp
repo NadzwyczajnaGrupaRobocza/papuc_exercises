fab7170be3e574a74fc727204a268f2e492f4189
Feldmarszalekduda <Feldmarszalekduda@gmail.com>
2016-07-09 07:09:05 +0200

    Added common functions

---
+++ mb/common/MB_Common.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+
+#include "MB_Common.hpp"
+
+namespace mbcommon
+{
+
+bool doublesAreEqual(const double& first, const double& second, const double& epsilon = 0.00000001)
+{
+    if (std::fabs(first - second) < epsilon)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+} //namespace mbcommon
---
+++ mb/common/MB_Common.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace mbcommon
+{
+
+bool doublesAreEqual(const double&, const double&, const double&);
+
+} //namespace mbcommon
