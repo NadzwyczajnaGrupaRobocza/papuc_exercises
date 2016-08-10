32bf35959aebdfb5955d32827deff0df0b6c2c5c
Feldmarszalekduda <Feldmarszalekduda@gmail.com>
2016-08-10 20:01:45 +0200

    Changes in Common

--- mb/common/MB_Common.cpp
+++ mb/common/MB_Common.cpp
@@ -5,8 +5,12 @@
 namespace mbcommon
 {
 
+<<<<<<< 2ba5851e562b94f23865961ca33e4222a99c0832
 bool doublesAreEqual(const double& first, const double& second,
                      const double& epsilon)
+=======
+bool doublesAreEqual(const double& first, const double& second, const double& epsilon)
+>>>>>>> fix in definition and declaration
 {
     if (std::fabs(first - second) < epsilon)
     {
