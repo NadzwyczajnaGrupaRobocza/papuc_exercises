58c1267d5ea8d2b3ec2b71dcac1532f2482c712c
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-10-04 19:19:09 +0200

    [LM][ch06-07] Shunting Yard based AST building
    First part of Shunting Yard algorithm implementation
    for representing calculator inputs.

--- lm/ch06/MockTokenStream.hpp
+++ lm/ch06/MockTokenStream.hpp
@@ -24,8 +24,14 @@ public:
         _putback(t);
     }
 
+    bool hasNext() override
+    {
+        return _hasNext();
+    }
+
     MOCK_METHOD0(_get, Token());
     MOCK_METHOD1(_putback, void(Token));
+    MOCK_METHOD0(_hasNext, bool());
 
 private:
     std::stringstream s;
--- lm/ch06/Token.cpp
+++ lm/ch06/Token.cpp
@@ -3,35 +3,47 @@
 
 namespace calc
 {
-Token::Token()
-    : typeId{0}, value{0}
+Token::Token() : typeId{0}, value{0}, id{""}
 {
 }
 
-Token::Token(char typeIdInit)
-    : typeId{typeIdInit}, value{0}
+Token::Token(char typeIdInit) : typeId{typeIdInit}, value{0}, id{""}
 {
 }
 
 Token::Token(char typeIdInit, double valueInit)
-    : typeId{typeIdInit}, value{valueInit}
+    : typeId{typeIdInit}, value{valueInit}, id{""}
+{
+}
+
+Token::Token(char typeIdInit, const std::string& idInit)
+
+    : typeId{typeIdInit}, value{0}, id{idInit}
 {
 }
 
 std::ostream& operator<<(std::ostream& out, const Token& t)
 {
-    if (t.typeId != '8')
+    switch (t.typeId)
     {
-        return out << "TOK-s:" << t.typeId;
+    case '8':
+    {
+        return out << "TOK-val:" << t.value;
     }
-    else
+    case 'S':
     {
-        return out << "TOK-v:" << t.value;
+        return out << "TOK-str:" << t.id;
+    }
+    default:
+    {
+        return out << "TOK-sym:" << t.typeId;
+    }
     }
 }
 
 bool operator==(const Token& lhs, const Token& rhs)
 {
-    return lhs.typeId == rhs.typeId && lhs.value == rhs.value;
+    return lhs.typeId and rhs.typeId && lhs.value == rhs.value and
+           lhs.id == rhs.id;
 }
 }
