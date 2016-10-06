3ca136f314873524203503ad591337485b307554
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-10-07 00:53:42 +0200

    [LM] shunting yard unary operators implementation

--- lm/ch06/ShuntingYard.cpp
+++ lm/ch06/ShuntingYard.cpp
@@ -24,6 +24,38 @@ ASTNode::ASTNode(Token&& tokenInit, std::unique_ptr<ASTNode> leftInit,
 {
 }
 
+std::string lispyTreePrint(ASTNode* root)
+{
+    std::stringstream output;
+
+    if (root != nullptr)
+    {
+        if (root->data.typeId != '8' and root->data.typeId != 'S')
+        {
+            output << '(';
+            output << root->data.typeId << ' ';
+            if (root->left != nullptr)
+            {
+                output << lispyTreePrint(root->left.get()) << ' ';
+            }
+            if (root->right != nullptr)
+            {
+                output << lispyTreePrint(root->right.get());
+            }
+            output << ')';
+        }
+        else if (root->data.typeId == '8')
+        {
+            output << root->data.value;
+        }
+        else if (root->data.typeId == 'S')
+        {
+            output << root->data.id;
+        }
+    }
+    return output.str();
+}
+
 ASTBuilder::ASTBuilder(util::Log& logInit, TokenStream& tsInit)
     : log{logInit}, ts{tsInit}
 {
@@ -37,47 +69,102 @@ ASTBuilder::ASTBuilder(util::Log& logInit, TokenStream&& tsInit)
 
 std::unique_ptr<ASTNode> ASTBuilder::getAST()
 {
+    bool binaryOpExpectedNext = false;
+
     while (ts.hasNext())
     {
         auto token = ts.get();
         log.debug() << "processing token: " << token << "\n";
+
         switch (token.typeId)
         {
         case '8':
         case 'S':
         {
             outStack.push(std::make_unique<ASTNode>(token));
+            binaryOpExpectedNext = true;
             break;
         }
         case '+':
         case '-':
         {
-            while (not opStack.empty() and
-                   (opStack.top().typeId == '*' or opStack.top().typeId == '/'))
+            if (binaryOpExpectedNext)
             {
-                processBinaryPop(opStack.top());
-                opStack.pop();
-            }
+                while (not opStack.empty() and
+                       hasLowerPrecedenceThanStackTop(token))
+                {
+                    if (isBinaryOp(opStack.top()))
+                    {
+                        processBinaryPop(opStack.top());
+                    }
+                    else if (isUnaryOp(opStack.top()))
+                    {
+                        processUnaryPop(opStack.top());
+                    }
+                    opStack.pop();
+                }
 
-            opStack.push(token);
+                opStack.push(token);
+                binaryOpExpectedNext = false;
+            }
+            else
+            {
+                if (token.typeId == '-')
+                {
+                    opStack.push(Token{'#'});
+                }
+            }
             break;
         }
         case '*':
         case '/':
         {
+            while (not opStack.empty() and
+                   hasLowerPrecedenceThanStackTop(token))
+            {
+                processOp(opStack.top());
+                opStack.pop();
+            }
+
             opStack.push(token);
+            binaryOpExpectedNext = false;
+            break;
+        }
+        case '(':
+        {
+            opStack.push(token);
+            binaryOpExpectedNext = false;
+            break;
+        }
+        case ')':
+        {
+            while (not opStack.empty() and opStack.top().typeId != '(')
+            {
+                processOp(opStack.top());
+                opStack.pop();
+            }
+
+            if (opStack.empty() or opStack.top().typeId != '(')
+            {
+                log.error() << "no matching paren found on stack\n";
+            }
+            else
+            {
+                opStack.pop();
+            }
+            binaryOpExpectedNext = true;
             break;
         }
         default:
         {
-            log.error() << "default should not happen\n";
+            log.error() << "unhandled token type\n";
         }
         }
     }
 
     while (not opStack.empty())
     {
-        processBinaryPop(opStack.top());
+        processOp(opStack.top());
         opStack.pop();
     }
 
@@ -87,14 +174,90 @@ std::unique_ptr<ASTNode> ASTBuilder::getAST()
     }
     else
     {
+        log.error() << "algorithm done with more than one element on the "
+                       "output stack\n";
         return nullptr;
+    }
+}
+
+void ASTBuilder::processOp(const Token& opTok)
+{
+    if (isBinaryOp(opTok))
+    {
+        processBinaryPop(opTok);
+    }
+    else if (isUnaryOp(opTok))
+    {
+        processUnaryPop(opTok);
+    }
+    else
+    {
+        throw std::logic_error{"unclassified token encountered"};
+    }
+}
+
+bool ASTBuilder::hasLowerPrecedenceThanStackTop(const Token& opTok)
+{
+    auto incomming = opTok.typeId;
+    auto stackTop = opStack.top().typeId;
+    if (incomming == '+' or incomming == '-')
+    {
+        if (stackTop == '*' or stackTop == '/' or stackTop == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else if (incomming == '*' or incomming == '/')
+    {
+        if (stackTop == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool ASTBuilder::isBinaryOp(const Token& opTok)
+{
+    switch (opTok.typeId)
+    {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+    {
+        return true;
+    }
+    default:
+    {
+        return false;
+    }
     }
 }
 
 void ASTBuilder::processBinaryPop(const Token& opTok)
 {
+    if (not isBinaryOp(opTok.typeId))
+    {
+        log.error() << "processing non binary " << opTok.typeId
+                    << " as binary operator!\n";
+    }
+
     if (outStack.size() < 2u)
     {
+        log.error() << lispyTreePrint(outStack.top().get()) << '\n';
         throw std::logic_error("malformed expression");
     }
     else
@@ -106,6 +269,42 @@ void ASTBuilder::processBinaryPop(const Token& opTok)
 
         outStack.push(std::make_unique<ASTNode>(opTok, std::move(leftArg),
                                                 std::move(rightArg)));
+    }
+}
+
+bool ASTBuilder::isUnaryOp(const Token& opTok)
+{
+    switch (opTok.typeId)
+    {
+    case '#':
+    {
+        return true;
+    }
+    default:
+    {
+        return false;
+    }
+    }
+}
+
+void ASTBuilder::processUnaryPop(const Token& opTok)
+{
+    if (not isUnaryOp(opTok.typeId))
+    {
+        log.error() << "processing non unary " << opTok.typeId
+                    << " as unary operator!\n";
+    }
+
+    if (outStack.size() < 1u)
+    {
+        throw std::logic_error("malformed expression");
+    }
+    else
+    {
+        auto arg = std::move(outStack.top());
+        outStack.pop();
+        outStack.push(
+            std::make_unique<ASTNode>(opTok, nullptr, std::move(arg)));
     }
 }
 }
