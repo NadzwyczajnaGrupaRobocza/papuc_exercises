1e924874f93e6143d2d87d8756ebe5bc70648b7e
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-10-11 20:08:51 +0200

    [ch06-07] move ast node to separate file

--- lm/ch07/ShuntingYard.cpp
+++ lm/ch07/ShuntingYard.cpp
@@ -2,28 +2,6 @@
 
 namespace calc
 {
-ASTNode::ASTNode(const Token& tokenInit)
-    : data{tokenInit}, left{nullptr}, right{nullptr}
-{
-}
-
-ASTNode::ASTNode(Token&& tokenInit) : data{std::move(tokenInit)}
-{
-}
-
-ASTNode::ASTNode(const Token& tokenInit, std::unique_ptr<ASTNode> leftInit,
-                 std::unique_ptr<ASTNode> rightInit)
-    : data{tokenInit}, left{std::move(leftInit)}, right{std::move(rightInit)}
-{
-}
-
-ASTNode::ASTNode(Token&& tokenInit, std::unique_ptr<ASTNode> leftInit,
-                 std::unique_ptr<ASTNode> rightInit)
-    : data{std::move(tokenInit)}, left{std::move(leftInit)},
-      right{std::move(rightInit)}
-{
-}
-
 std::string lispyTreePrint(ASTNode* root)
 {
     std::stringstream output;
