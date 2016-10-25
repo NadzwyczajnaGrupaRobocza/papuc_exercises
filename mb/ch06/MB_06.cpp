a780b0be4a33ee50c6e110c002446a2e30c65f80
Feldmarszalekduda <Feldmarszalekduda@gmail.com>
2016-10-25 20:12:44 +0200

    Splitting body of main into functions

--- mb/ch06/MB_06.cpp
+++ mb/ch06/MB_06.cpp
@@ -5,51 +5,55 @@
 #include "TokenStream.hpp"
 #include "Calculator.hpp"
 
-int main(int argc, char* argv[])
+void calculate(ITokenStream& tokenStream)
+{
+    double value = 0;
+    Calculator parser{tokenStream};
+    while (std::cin)
+    {
+        Token token = tokenStream.get();
+        while (token.kind == END_OF_EXPR)
+        {
+            token = tokenStream.get();
+        }
+        if(token.kind == QUIT)
+        {
+            return;
+        }
+        tokenStream.putback(token);
+        value = parser.expression();
+        std::cout << "= " << value << std::endl;
+    }
+}
+
+TokenStream passArgsToTokenStream(const char* argv[])
+{
+    std::cout << ">> " << argv[1] << std::endl;
+    
+    std::string expression{argv[1]};
+    TokenStream tokenStream{expression + END_OF_EXPR + QUIT};
+    return tokenStream;
+}
+
+int main(int argc, const char* argv[])
 {
     if(argc < 2)
     {
         std::cout << "There aren't any operations send to Calculator" << std::endl;
         return 1;
     }
-    
-    std::cout << ">> " << argv[1] << std::endl;
-    
-    std::string expression{argv[1]};
-    TokenStream tokenStream{expression + END_OF_EXPR + QUIT};
 
+    TokenStream tokenStream = passArgsToTokenStream(argv);
+    
     try
     {
-        double value = 0;
-        Calculator parser{tokenStream};
-        while (std::cin)
-        {
-            try
-            {
-                Token token = tokenStream.get();
-                while (token.kind == END_OF_EXPR)
-                {
-                    token = tokenStream.get();
-                }
-                if(token.kind == QUIT)
-                {
-                    return 0;
-                }
-                tokenStream.putback(token);
-                value = parser.expression();
-                std::cout << "= " << value << std::endl;
-            }
-            catch (std::logic_error& error)
-            {
-                std::cerr << error.what() << std::endl;
-                clean(tokenStream);
-            }
-        }
+        calculate(tokenStream);
         return 0;
     }
     catch (std::logic_error& error)
     {
         std::cerr << error.what() << std::endl;
+        clean(tokenStream);
         return 1;
     }
     catch (...)
@@ -57,6 +61,6 @@ int main(int argc, char* argv[])
         std::cerr << "Unexpected exception";
         return 2;
     }
-    return 0;
+    
 }
 
