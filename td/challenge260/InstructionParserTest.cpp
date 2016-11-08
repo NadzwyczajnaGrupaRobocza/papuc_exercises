0c4cc8000202abf551fa741294a21ac5bda9d795
kn65op <kn65op@gmail.com>
2016-11-08 19:35:21 +0100

    InstructionParser is able to reject instruction

--- td/challenge260/InstructionParserTest.cpp
+++ td/challenge260/InstructionParserTest.cpp
@@ -1,20 +1,27 @@
 //#include "InstructionParser.h"
 
 #include <string>
+#include <stdexcept>
 
 #include "gtest/gtest.h"
 
 class InstructionParser
 {
 public:
-  void parseInstructions(const std::string &)
+  class UnknownInstruction : public std::runtime_error
   {
-
+  public:
+    UnknownInstruction(const std::string& msg) : std::runtime_error{msg}
+    {}
+  };
+  void parseInstructions(const std::string& instruction)
+  {
+    throw UnknownInstruction{"Unknown instruction" + instruction};
   }
 };
 
-TEST(InstructionParser, ParserShouldAcceptAnyIntruction)
+TEST(InstructionParser, ParserShouldDeclineUnknownInstruction)
 {
   InstructionParser parser;
-  parser.parseInstructions("Instructions");
+  EXPECT_THROW(parser.parseInstructions("Instructions"), InstructionParser::UnknownInstruction);
 }
