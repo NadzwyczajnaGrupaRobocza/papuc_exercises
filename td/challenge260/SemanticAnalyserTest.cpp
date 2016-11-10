a3afb7a9d3a0383cc0a2e24580d9284437d0ae5d
kn65op <kn65op@gmail.com>
2016-11-10 19:34:04 +0100

    Analyser reject only Ld token

--- td/challenge260/SemanticAnalyserTest.cpp
+++ td/challenge260/SemanticAnalyserTest.cpp
@@ -5,15 +5,27 @@
 class SemanticAnalyser
 {
 public:
-  bool analyse(const Tokens&)
+  bool analyse(const Tokens& tokens)
   {
-    return true;
+    return tokens.empty();
   }
 };
+
+constexpr Token createTokenWithZeroValue(TokenType type)
+{
+  return {type, 0};
+}
 
 TEST(SemanticAnalyserTest, ShouldAcceptEmptyTokens)
 {
   SemanticAnalyser analyser;
-  Tokens tokens{{}};
+  Tokens tokens{};
   ASSERT_TRUE(analyser.analyse(tokens));
+}
+
+TEST(SemanticAnalyserTest, ShouldNotAcceptInvalidInstructionSet)
+{
+  SemanticAnalyser analyser;
+  Tokens tokens{createTokenWithZeroValue(TokenType::Ld)};
+  ASSERT_FALSE(analyser.analyse(tokens));
 }
