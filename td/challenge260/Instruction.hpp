eb749d4d53e5f90be3925770947ba2a35b5a621d
kn65op <kn65op@gmail.com>
2016-11-10 20:30:49 +0100

    Add Instruction

--- td/challenge260/Instruction.hpp
+++ td/challenge260/Instruction.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
-enum class Intruction
+enum class IntructionType
 {
   LdA,
   OutA
+};
+
+struct Instruction
+{
+  using ValueType = unsigned;
+  InstructionType type,
+  ValueType value;
 };
