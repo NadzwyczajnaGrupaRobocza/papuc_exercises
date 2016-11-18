a90e772ca3d5a786f2f1ff101c846062dd7b9129
Abergard <littlinker@gmail.com>
2016-11-18 23:37:18 +0100

    travis test

--- lm/ch04/Expression.cpp
+++ lm/ch04/Expression.cpp
@@ -13,15 +13,21 @@ Expression::Expression(const std::string& inputInit) : input{inputInit}
 }
 
 double Expression::getResult()
-{
+{        std::cout << "get result" << std::endl;
     std::istringstream istr(input);
+    std::cout << "after istr" << std::endl;
 
+            std::cout << "val init" << std::endl;
     double val1 = 0.0;
     double val2 = 0.0;
     char op = '\0';
+    std::cout << "before >> " << istr << std::endl;
     istr >> val1;
+    std::cout << "after val1 >> " << val1 << std::endl;
     istr >> op;
+    std::cout << "after op >> " << op << std::endl;
     istr >> val2;
+    std::cout << "after val2 >> " << val2 << std::endl;
     switch (op)
     {
     case '+': std::cout << "1" << std::endl; return val1 + val2;
