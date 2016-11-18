21b32266773d7a1cc706214885e5d47442decf09
Abergard <littlinker@gmail.com>
2016-11-18 23:25:45 +0100

    travis test

--- lm/ch04/Expression.cpp
+++ lm/ch04/Expression.cpp
@@ -1,7 +1,9 @@
 #include "Expression.hpp"
 
-#include <sstream>
 #include <cmath>
+#include <sstream>
+
+#include <iostream>
 
 namespace lm
 {
@@ -19,18 +21,25 @@ double Expression::getResult()
     istr >> val1;
     istr >> op;
     istr >> val2;
-
     switch (op)
     {
-    case '+': return val1 + val2;
-    case '-': return val1 - val2;
-    case '*': return val1 * val2;
+    case '+': std::cout << "1" << std::endl; return val1 + val2;
+    case '-': std::cout << "2" << std::endl; return val1 - val2;
+    case '*': std::cout << "3" << std::endl; return val1 * val2;
     case '/':
-        if (std::fabs(val2) < epsilon)
-            throw std::runtime_error("division by zero");
 
+        std::cout << "4" << std::endl;
+        if (std::fabs(val2) < epsilon)
+        {
+            std::cout << "5" << std::endl;
+            throw std::runtime_error("division by zero");
+        }
+
+        std::cout << "6" << std::endl;
         return val1 / val2;
-    default: throw std::runtime_error("unknown operation");
+    default:
+        std::cout << "7" << std::endl;
+        throw std::runtime_error("unknown operation");
     }
 }
 }
