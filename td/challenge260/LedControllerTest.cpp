2d5c94a02083b0628f0de48a4134c87164c28b73
kn65op <kn65op@gmail.com>
2016-11-20 13:11:45 +0100

    LedController can run programs

--- td/challenge260/LedControllerTest.cpp
+++ td/challenge260/LedControllerTest.cpp
@@ -1,11 +1,18 @@
 #include "gtest/gtest.h"
 
+#include "Instruction.hpp"
+
 class LedController
 {
+public:
+  void runProgram(const Instructions &)
+  {
+  }
 
 };
 
 TEST(LedControllerTest, ShouldCreate)
 {
   LedController controller;
+  controller.runProgram(Instructions{});
 }
