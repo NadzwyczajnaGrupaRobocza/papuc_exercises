9790c0b90f82b873a962b57461ba0ecc1aa188c9
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-11-24 20:57:49 +0100

    [game01] trival board drawing class

--- lm/game01/main.cpp
+++ lm/game01/main.cpp
@@ -1,19 +1,24 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
-using namespace std;
+#include <iostream>
+#include "Board.hpp"
 
-int main(int /*argc*/, char** /*argv*/) {
+int main(int /*argc*/, char** /*argv*/)
+{
 
-  /* Code adapted from the SFML 2 "Window" example */
+    sf::RenderWindow mainWindow(sf::VideoMode(800, 600), "myproject");
 
-  sf::Window App(sf::VideoMode(800, 600), "myproject");
-
-  while (App.isOpen()) {
-    sf::Event Event;
-    while (App.pollEvent(Event)) {
-      if (Event.type == sf::Event::Closed)
-    App.close();
+    while (mainWindow.isOpen())
+    {
+        lmg01::Board b;
+        b.drawOn(mainWindow);
+        sf::Event event;
+        while (mainWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                mainWindow.close();
+            }
+        }
+        mainWindow.display();
     }
-    App.display();
-  }
 }
