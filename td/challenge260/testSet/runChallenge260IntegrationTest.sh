3836d5c1c9f7254897bc23e8ee0621b34c922dab
kn65op <kn65op@gmail.com>
2016-11-29 23:50:41 +0100

    Imporove integration tests

--- td/challenge260/testSet/runChallenge260IntegrationTest.sh
+++ td/challenge260/testSet/runChallenge260IntegrationTest.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-$1 < input.txt > output.txt
-diff output.txt expected_output.txt
+testsAllCount=0
+testsFailCount=0
 
-exit $?
+for inputFile  in input*
+do
+	fileNumberWithInput=${inputFile%.*}
+	fileNumber=${fileNumberWithInput#*_}
+	expectedOutputFile="expected_output_${fileNumber}.txt"
+	outputFile="output_${fileNumber}.txt"
+	diffResultFile="diff_${fileNumber}.txt"
+	$1 < $inputFile > $outputFile
+	diff $outputFile $expectedOutputFile > $diffResultFile
+	if [ $? -ne 0 ]
+	then
+		echo "Test failed for ${inputFile} : ${expectedOutputFile} Check ${diffResultFile}"
+		let "testFailCount++"
+	fi
+	let "testAllCount++"
+done
+
+if [ $testsFailCount -ne 0 ]
+then
+	echo "${testFailCount}/${testAllCount} TESTS FAILED"
+	exit -1
+else
+	echo "${testAllCount} TESTS PASSED"
+	exit 0
+fi
