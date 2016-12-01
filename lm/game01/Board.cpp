9802043d1c7802238eaf08055d9638abb57c0cdb
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-12-02 00:51:01 +0100

    [lm-game01] adding randomly generated obstacles

--- lm/game01/Board.cpp
+++ lm/game01/Board.cpp
@@ -3,7 +3,9 @@
 
 namespace lmg01
 {
-Board::Board(int maxX, int maxY) : N{maxX}, M{maxY}
+Board::Board(const sf::View& initialView) : currentView{initialView},
+                                            N{static_cast<int>(currentView.getSize().x / 50.f) + 1},
+                                            M{static_cast<int>(currentView.getSize().y / 50.f) + 1}
 {
 }
 
@@ -18,13 +20,13 @@ void Board::drawEmptyBoard(sf::RenderWindow& target)
     {
         for (auto j = 0; j < M; j++)
         {
-            float x_pos = 50.0f * static_cast<float>(i);
-            float y_pos = 50.0f * static_cast<float>(j);
+            float x_pos = 50.f * static_cast<float>(i);
+            float y_pos = 50.f * static_cast<float>(j);
             sf::RectangleShape rectangle;
             rectangle.setSize(sf::Vector2f(50.0f, 50.0f));
             rectangle.setOutlineColor(sf::Color::Red);
             rectangle.setOutlineThickness(1);
-            rectangle.setPosition(50.0f + x_pos, 50.0f + y_pos);
+            rectangle.setPosition(x_pos, y_pos);
             target.draw(rectangle);
         }
     }
--- lm/game01/main.cpp
+++ lm/game01/main.cpp
@@ -1,17 +1,17 @@
+#include "World.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include "World.hpp"
 
 int main(int /*argc*/, char** /*argv*/)
 {
-
-    sf::RenderWindow mainWindow(sf::VideoMode(800, 600), "myproject");
+    sf::Vector2i screenSize{800, 600};
+    sf::RenderWindow mainWindow(sf::VideoMode(screenSize.x, screenSize.y), "myproject");
     mainWindow.setVerticalSyncEnabled(true);
     lmg01::World w(mainWindow);
 
     sf::Clock frameClock;
 
-    float speed = 40.f;
+    float baseSpeed = 200.f;
 
     while (mainWindow.isOpen())
     {
@@ -22,28 +22,33 @@ int main(int /*argc*/, char** /*argv*/)
             {
                 mainWindow.close();
             }
-
-            sf::Time frameTick = frameClock.restart();
-
-            sf::Vector2f movement{0.f, 0.f};
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            {
-                movement.y -= speed;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            {
-                movement.y += speed;
-            }
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-                movement.x -= speed;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            {
-                movement.x += speed;
-            }
-
-            w.advance(frameTick, movement);
         }
+
+        sf::Time frameTick = frameClock.restart();
+        float speed{baseSpeed};
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+        {
+            speed *= .5f;
+        }
+
+        sf::Vector2f movement{0.f, 0.f};
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            movement.y -= speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            movement.y += speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            movement.x -= speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            movement.x += speed;
+        }
+        w.advance(frameTick, movement);
     }
 }
