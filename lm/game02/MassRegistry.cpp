0541ed2b9b77b9e410ce5bd7af60881145e5bc3b
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-12-05 23:27:05 +0100

    [lm-game02] refactor boundry repulsion

--- lm/game02/MassRegistry.cpp
+++ lm/game02/MassRegistry.cpp
@@ -7,51 +7,58 @@ void MassRegistry::add_entity(PhysicalEntity* e)
 {
     entities.push_back(e);
 }
+sf::Vector2f compute_boundry_accel(float pos_dif,
+                                   const sf::Vector2f& velocity);
+sf::Vector2f compute_boundry_accel(float pos_dif,
+                                   const sf::Vector2f& velocity)
+{
+    auto factor = pos_dif > std::numeric_limits<float>::epsilon() ? 1000.f / (pos_dif * pos_dif) : 100000.f;
+    return velocity * (-1.f) * factor;
+}
+sf::Vector2f compute_central_mass_accel(const sf::Vector2f& position);
+sf::Vector2f compute_central_mass_accel(const sf::Vector2f& position)
+{
+    sf::Vector2f large_mass_center{400.f, 300.f};
+
+    sf::Vector2f direction = large_mass_center - position;
+    float length = std::hypot(direction.x, direction.y);
+
+    float G = 0.6f;
+    float large_mass = 2000000.f;
+
+    return G * large_mass * direction * (1 / (length * length * length));
+
+}
 
 sf::Vector2f MassRegistry::get_accel(const PhysicalEntity* const caller,
                                      float /*dt*/) const
 {
     const auto& caller_state = caller->get_state();
-    // constexpr auto friction_coeff = 1.f;
     sf::Vector2f base_accel{0.f, 0.f};
-    sf::Vector2f large_mass_center{400.f, 300.f};
 
-    sf::Vector2f direction = large_mass_center - caller_state.position;
-    float length = std::hypot(direction.x, direction.y);
-    sf::Vector2f accel_from_mass =
-        direction * (1 / (length * length * length));
-    float G = 0.6f;
-    float large_mass = 2000000.f;
-    accel_from_mass *= G * large_mass;
-    base_accel += accel_from_mass;
+    base_accel += compute_central_mass_accel(caller_state.position);
 
-    if (caller_state.position.x > 700.f)
+    if (caller_state.position.x > 600.f)
     {
-        auto pos_dif = 800.f - caller_state.position.x;
-        auto factor = 100.f / (pos_dif * pos_dif);
-        base_accel += (-1.f) * factor * caller_state.velocity;
-        //(-1.f / pos_dif) * friction_coeff * caller_state.velocity;
+        base_accel += compute_boundry_accel(800.f - caller_state.position.x,
+                                            caller_state.velocity);
     }
-    if (caller_state.position.x < 100.f)
+    if (caller_state.position.x < 200.f)
     {
-        auto pos_dif = caller_state.position.x;
-        auto factor = 100.f / (pos_dif * pos_dif);
-        base_accel += (-1.f) * factor * caller_state.velocity;
+        base_accel += compute_boundry_accel(caller_state.position.x,
+                                            caller_state.velocity);
     }
-    if (caller_state.position.y > 500.f)
+    if (caller_state.position.y > 400.f)
     {
-        auto pos_dif = 600.f - caller_state.position.y;
-        auto factor = 100.f / (pos_dif * pos_dif);
-        base_accel += (-1.f) * factor * caller_state.velocity;
+        base_accel += compute_boundry_accel(600.f - caller_state.position.y,
+                                            caller_state.velocity);
     }
-    if (caller_state.position.y < 50.f)
+    if (caller_state.position.y < 200.f)
     {
-        auto pos_dif = caller_state.position.y;
-        auto factor = 100.f / (pos_dif * pos_dif);
-        base_accel += (-1.f) * factor * caller_state.velocity;
+        base_accel += compute_boundry_accel(caller_state.position.y,
+                                            caller_state.velocity);
     }
 
     return base_accel;
-    ;
 }
 }
