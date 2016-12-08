25f73205cc764f889c57835d7a0e4c447bd92e20
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2016-12-08 20:08:21 +0100

    [lm-game01] refactoring

--- lm/game01/World.hpp
+++ lm/game01/World.hpp
@@ -8,17 +8,17 @@ namespace lmg01
 class World
 {
 public:
-    World(sf::RenderWindow& target);
+    World(sf::View);
     void advance(const sf::Time& tick, const sf::Vector2f& playerMove);
+    void draw(sf::RenderTarget&);
 private:
     std::vector<sf::CircleShape> createObstacles();
-    void draw();
-    void drawObstacles();
-    void drawPlayer();
+    void drawObstacles(sf::RenderTarget&);
+    void drawPlayer(sf::RenderTarget&);
     void movePlayer(const sf::Vector2f&);
     sf::Vector2f validMovement(const sf::Vector2f&, const sf::CircleShape& obstacle) const;
 
-    sf::RenderWindow& target;
+    sf::View current_view;
     Board b;
     sf::CircleShape player;
     sf::Vector2f player_position;
