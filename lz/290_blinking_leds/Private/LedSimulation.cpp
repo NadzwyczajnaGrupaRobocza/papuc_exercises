f95ccc36b46f234b4b2e1518f9a596d8d21305e3
Abergard <littlinker@gmail.com>
2016-12-25 12:01:07 +0100

    fix explicit LedToken

--- lz/290_blinking_leds/Private/LedSimulation.cpp
+++ lz/290_blinking_leds/Private/LedSimulation.cpp
@@ -1,16 +1,17 @@
 #include "LedSimulation.hpp"
 
+#include <boost/range/algorithm/for_each.hpp>
 #include <iostream>
 #include <iterator>
 #include <sstream>
 #include <string>
-#include <boost/range/algorithm/for_each.hpp>
 
 #include "LedSimulationFsm.hpp"
 
 namespace lz
 {
-LedSimulation::LedSimulation(std::unique_ptr<ILedSimulationFsm> ledSimulationFsm)
+LedSimulation::LedSimulation(
+    std::unique_ptr<ILedSimulationFsm> ledSimulationFsm)
     : fsm{std::move(ledSimulationFsm)}
 {
 }
@@ -22,8 +23,13 @@ void LedSimulation::readCommands(std::istream& input)
     std::istream_iterator<char> it(input);
     std::string sentence(it, std::istream_iterator<char>{});
     std::istringstream iss(sentence);
-    std::copy(std::istream_iterator<std::string>{iss},
-              std::istream_iterator<std::string>{}, std::back_inserter(tokens));
+    // std::copy(std::istream_iterator<std::string>{iss},
+    //           std::istream_iterator<std::string>{}, std::back_inserter(tokens));
+
+    std::transform(
+        std::istream_iterator<std::string>{iss},
+        std::istream_iterator<std::string>{}, std::back_inserter(tokens),
+        [](auto&& str_token) { return LedToken{std::move(str_token)}; });
 }
 
 void LedSimulation::run()
