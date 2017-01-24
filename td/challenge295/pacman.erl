f78a3cc8ef63e814516bdb8028b8aa7bc5cf5187
kn65op <kn65op@gmail.com>
2017-01-24 19:59:40 +0100

    Reads input

--- td/challenge295/pacman.erl
+++ td/challenge295/pacman.erl
@@ -1,11 +1,34 @@
 -module(pacman).
 -export([getMaxPacgums/0]).
+-export([parseLine/1]).
+
 
 getNumberOfMoves({ok, [N]}) ->
 	N;
 
 getNumberOfMoves(_) ->
 	invalid_number_of_moves.
+
+
+
+parseLine([$X | Tail]) ->
+	[wall | parseLine(Tail)];
+
+parseLine([$C | Tail]) ->
+	[pacman | parseLine(Tail)];
+
+parseLine([String | Tail]) ->
+	Integer = case string:to_integer([String]) of
+			{Int, _} ->
+				  Int;
+			  _ ->
+				  0
+		  end,
+	[Integer | parseLine(Tail)];
+
+parseLine(_) ->
+	[].
+
 
 getFields(eof) ->
 	%io:format("Get Fields empty~n", []),
@@ -20,11 +43,14 @@ getFields(Line) ->
 	%io:format("Get Fields line read~n", []),
 	%io:format(standard_error, "~s~n", [Line]),
 	LineLen = string:len(Line) - 1,
-	[string:left(Line, LineLen) | getFields()].
+	LineInAtoms = parseLine(string:left(Line, LineLen)),
+	[LineInAtoms | getFields()].
+
 
 getFields() ->
 	%io:format(standard_error, "Get", []),
 	getFields(io:get_line("")).
+
 
 handleNumberOfMoves(invalid_number_of_moves) ->
 	void;
@@ -32,8 +58,10 @@ handleNumberOfMoves(invalid_number_of_moves) ->
 handleNumberOfMoves(_) ->
 	getFields().
 
+
 getMaxPacgums() ->
 	N = getNumberOfMoves(io:fread("", "~d")),
 	io:format("N:~w~n", [N]),
 	Fields = handleNumberOfMoves(N),
 	io:format("Readed fields ~p~n", [Fields]).
+
