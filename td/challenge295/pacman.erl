16e92573039e22205ccbe77a6b1b19ae9d8762c9
kn65op <kn65op@gmail.com>
2017-01-24 19:19:07 +0100

    Read works

--- td/challenge295/pacman.erl
+++ td/challenge295/pacman.erl
@@ -7,8 +7,24 @@ getNumberOfMoves({ok, [N]}) ->
 getNumberOfMoves(_) ->
 	invalid_number_of_moves.
 
+getFields(eof) ->
+	%io:format("Get Fields empty~n", []),
+	%io:format(standard_error, "EOF", []),
+	[];
+
+getFields({error, Info}) ->
+	io:format("Get Fields error: ~p~n", [Info]);
+	%io:format(standard_error, "Erros~n", []).
+
+getFields(Line) ->
+	%io:format("Get Fields line read~n", []),
+	%io:format(standard_error, "~s~n", [Line]),
+	LineLen = string:len(Line) - 1,
+	[string:left(Line, LineLen) | getFields()].
+
 getFields() ->
-	io:fread("", "~s").
+	%io:format(standard_error, "Get", []),
+	getFields(io:get_line("")).
 
 handleNumberOfMoves(invalid_number_of_moves) ->
 	void;
@@ -19,4 +35,5 @@ handleNumberOfMoves(_) ->
 getMaxPacgums() ->
 	N = getNumberOfMoves(io:fread("", "~d")),
 	io:format("N:~w~n", [N]),
-	handleNumberOfMoves(N).
+	Fields = handleNumberOfMoves(N),
+	io:format("Readed fields ~p~n", [Fields]).
