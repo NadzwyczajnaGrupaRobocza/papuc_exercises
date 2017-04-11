a126f352852d63d47752c7e7b2504e8fb6dcc003
kn65op <kn65op@gmail.com>
2017-04-11 23:56:24 +0200

    Add fancy calculation

--- td/scheme-hello-world/hello.scm
+++ td/scheme-hello-world/hello.scm
@@ -8,3 +8,13 @@
 (define name "Another world")
 (display (string-append "Hello " name "!" ))
 (newline)
+
+(define (fancy_calulation a b c) 42)
+(display "Fancy calulation result: ")
+(display(fancy_calulation  "a" 10 4))
+(newline)
+
+(define (another_fancy_calulation a b c) (* b 42))
+(display "Another fancy calulation result: ")
+(display(another_fancy_calulation  "a" 10 4))
+(newline)
