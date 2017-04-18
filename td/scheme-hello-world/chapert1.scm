3b6ca17552ef13b4f264dec0d506740ecd6e882b
kn65op <kn65op@gmail.com>
2017-04-18 19:44:24 +0200

    Choose second and third, when they are bigger

--- td/scheme-hello-world/chapert1.scm
+++ td/scheme-hello-world/chapert1.scm
@@ -1,9 +1,12 @@
 (use-modules (unit-test))
 
 (define (square a) (* a a))
-(define (excercise1.1 a b c) (+ (square a)
-  (if (< b c) (square c)
-        (square b))))
+(define (excercise1.1 a b c)
+     (if (< b c)
+         (+ (square a) (square c))
+         (if (< a b)
+             (+ (square b) (square c))
+             (+ (square a) (square b)))))
 
 (assert-equal 0 (excercise1.1 0 0 0))
 (assert-equal 1 (excercise1.1 1 0 0))
@@ -11,3 +14,4 @@
 (assert-equal 5 (excercise1.1 2 1 0))
 (assert-equal 5 (excercise1.1 1 0 2))
 (assert-equal 13 (excercise1.1 2 1 3))
+(assert-equal 13 (excercise1.1 1 3 2))
