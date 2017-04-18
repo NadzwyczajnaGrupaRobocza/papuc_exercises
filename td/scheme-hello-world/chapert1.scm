a0163b34fee6aa6351d0ceac770b1c0fe1ed6875
kn65op <kn65op@gmail.com>
2017-04-18 19:47:31 +0200

    Choose second and third, when b is lower then c

--- td/scheme-hello-world/chapert1.scm
+++ td/scheme-hello-world/chapert1.scm
@@ -3,7 +3,9 @@
 (define (square a) (* a a))
 (define (excercise1.1 a b c)
      (if (< b c)
-         (+ (square a) (square c))
+         (if (< a  b)
+             (+ (square b) (square c))
+             (+ (square a) (square c)))
          (if (< a b)
              (+ (square b) (square c))
              (+ (square a) (square b)))))
@@ -15,3 +17,5 @@
 (assert-equal 5 (excercise1.1 1 0 2))
 (assert-equal 13 (excercise1.1 2 1 3))
 (assert-equal 13 (excercise1.1 1 3 2))
+(assert-equal 13 (excercise1.1 1 2 3))
+
