79f5514ac801db94d7bad243b9c7ed62352ef080
kn65op <kn65op@gmail.com>
2017-04-18 19:31:43 +0200

    Takes second and thirrd when first is 0

--- td/scheme-hello-world/chapert1.scm
+++ td/scheme-hello-world/chapert1.scm
@@ -1,10 +1,12 @@
 (use-modules (unit-test))
 
 (define (square a) (* a a))
-(define (excercise1.1 a b c) (+ (square a) (square b)))
+(define (excercise1.1 a b c)
+  (if (= a 0) (+ (square c) (square b))
+       (+ (square a) (square b))))
 
 (assert-equal 0 (excercise1.1 0 0 0))
 (assert-equal 1 (excercise1.1 1 0 0))
 (assert-equal 4 (excercise1.1 2 0 0))
 (assert-equal 5 (excercise1.1 2 1 0))
-(assert-equal 8 (excercise1.1 2 2 0))
+(assert-equal 5 (excercise1.1 0 2 1))
