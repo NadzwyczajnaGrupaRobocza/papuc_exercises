534620b29ced853ba127667f554b75c4377b25e0
kn65op <kn65op@gmail.com>
2017-04-18 19:25:47 +0200

    Returns square of first argument

--- td/scheme-hello-world/chapert1.scm
+++ td/scheme-hello-world/chapert1.scm
@@ -1,6 +1,8 @@
 (use-modules (unit-test))
 
-(define (excercise1.1 a b c) a)
+(define (square a) (* a a))
+(define (excercise1.1 a b c) (square a))
 
 (assert-equal 0 (excercise1.1 0 0 0))
 (assert-equal 1 (excercise1.1 1 0 0))
+(assert-equal 4 (excercise1.1 2 0 0))
