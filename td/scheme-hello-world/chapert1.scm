f97f6896cdef79e439289647f32f79771caffa38
kn65op <kn65op@gmail.com>
2017-04-18 19:26:47 +0200

    Returns square of first argument and adds second

--- td/scheme-hello-world/chapert1.scm
+++ td/scheme-hello-world/chapert1.scm
@@ -1,8 +1,9 @@
 (use-modules (unit-test))
 
 (define (square a) (* a a))
-(define (excercise1.1 a b c) (square a))
+(define (excercise1.1 a b c) (+ (square a) b))
 
 (assert-equal 0 (excercise1.1 0 0 0))
 (assert-equal 1 (excercise1.1 1 0 0))
 (assert-equal 4 (excercise1.1 2 0 0))
+(assert-equal 5 (excercise1.1 2 1 0))
