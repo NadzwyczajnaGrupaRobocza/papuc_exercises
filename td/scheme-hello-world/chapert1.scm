62f57a29aec8d721124e21230455687236a85e16
kn65op <kn65op@gmail.com>
2017-04-18 19:34:15 +0200

    Takes first and third, when second is less then third

--- td/scheme-hello-world/chapert1.scm
+++ td/scheme-hello-world/chapert1.scm
@@ -2,7 +2,7 @@
 
 (define (square a) (* a a))
 (define (excercise1.1 a b c)
-  (if (= a 0) (+ (square c) (square b))
+  (if (< b c) (+ (square a) (square c))
        (+ (square a) (square b))))
 
 (assert-equal 0 (excercise1.1 0 0 0))
@@ -10,3 +10,4 @@
 (assert-equal 4 (excercise1.1 2 0 0))
 (assert-equal 5 (excercise1.1 2 1 0))
 (assert-equal 5 (excercise1.1 1 0 2))
+(assert-equal 13 (excercise1.1 2 1 3))
